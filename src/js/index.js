19d3386701819069d11b356fb4bd23d1aa18ad56
Carlos Betancourt <146739071+DonBeta@users.noreply.github.com>
2024-01-11 20:31:56 +0000

    he terminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,44 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
+
+
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SecondCounter(props){
+    return(<div className="Bloque">
+        <div className="Reloj"> 
+            <i class="far fa-clock"></i>
+        </div>
+        <div className="FourIzquierda">{props.NumeroFour %10}</div>
+        <div className="Three">{props.NumeroThree %10}</div>
+        <div className="Two">{props.NumeroTwo %10}</div>
+        <div className="OneDerecha">{props.NumeroOne %10}</div>
+        
+    </div>);
+}
 
+SecondCounter.PropTypes ={
+    NumeroFour: PropTypes.number,
+    NumeroThree: PropTypes.number,
+    NumeroTwo: PropTypes.number,
+    NumeroOne: PropTypes.number
+} ;
+let counter = 0;
+setInterval(function(){
+    const FourIzquierda = Math.floor(counter / 1000);
+    const Three = Math.floor(counter / 100);
+    const Two = Math.floor(counter / 10);
+    const OneDerecha = Math.floor(counter / 1);
+    console.log(FourIzquierda, Three, Two, OneDerecha);
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+    counter++;
+ReactDOM.render(<SecondCounter NumeroOne={OneDerecha} NumeroTwo={Two} NumeroThree={Three} NumeroFour={FourIzquierda} />,
+ document.querySelector("#app"));
+
+
+},1000);
